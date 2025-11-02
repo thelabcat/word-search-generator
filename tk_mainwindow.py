92367cad9e8f7b0693f91f5fc6374519200f18ff
Wilbur Jaywright <zargulthewizard@outlook.com>
2025-11-01 23:10:08 -0400

    Completed #7 for Tk

--- tk_mainwindow.py
+++ tk_mainwindow.py
@@ -5,10 +5,10 @@ Present a GUI for the word search generator algorithm (Tkitner)
 
 This file is part of Word Search Generator.
 
-Word Search Generator is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
+Word Search Generator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
@@ -46,7 +46,11 @@ class TkWindow(tk.Tk):
         # Tkinter variables
         self.use_hard = tk.BooleanVar(self, True)
         self.size_fac_str = tk.StringVar(self, SIZE_FAC_DEFAULT)
-        self.size_fac_str.trace_add("write", lambda *args: self.verify_size_fac())
+        self.size_fac_str.trace_add(
+            "write",
+            lambda *args: self.verify_size_fac()
+            )
+        self.intersect_bias = tk.IntVar(self, 0)
 
         self.build()
         self.mainloop()
@@ -55,7 +59,11 @@ class TkWindow(tk.Tk):
         """Construct the GUI widgets"""
 
         # Checkbutton for using hard directions
-        ttk.Checkbutton(self, text="Use backwards directions", variable=self.use_hard).grid(row=0, sticky=tk.NSEW)
+        ttk.Checkbutton(
+            self,
+            text="Use backwards directions",
+            variable=self.use_hard
+            ).grid(row=0, sticky=tk.NSEW)
 
         # Number area for size_fac
         self.sf_frame = ttk.Frame(self)
@@ -63,30 +71,55 @@ class TkWindow(tk.Tk):
 
         ttk.Label(self.sf_frame, text="Size factor:", anchor=tk.E).grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.sf_spinbox = ttk.Spinbox(self.sf_frame, values=TK_SIZE_FAC_OPTIONS, textvariable=self.size_fac_str)
+        self.sf_spinbox = ttk.Spinbox(
+            self.sf_frame,
+            values=TK_SIZE_FAC_OPTIONS,
+            textvariable=self.size_fac_str
+            )
         self.sf_spinbox.grid(row=0, column=1, sticky=tk.NSEW)
 
-        self.sf_frame.columnconfigure(1, weight=1)  # Resize size factor frame around the column with the spinbox.
+        # Resize size factor frame around the column with the spinbox.
+        self.sf_frame.columnconfigure(1, weight=1)
+
+        # Intersection bias chooser
+        self.bias_frame = ttk.Frame(self)
+        self.bias_frame.grid(row=2, sticky=tk.NSEW, pady=3)
+        ttk.Label(self.bias_frame, text="Word intersections bias:")\
+            .grid(row=0, column=0, columnspan=3, sticky=tk.NSEW)
+
+        # Create radiobuttons for the three biases
+        for i, bias in enumerate(("Avoid", "Random", "Prefer")):
+            ttk.Radiobutton(
+                self.bias_frame,
+                text=bias,
+                variable=self.intersect_bias,
+                value=i - 1
+                ).grid(row=1, column=i, sticky=tk.NSEW, padx=7)
 
         # Entry area for the words, and a scrollbar
         self.entry_frame = ttk.Frame(self)
-        self.entry_frame.grid(row=2, sticky=tk.NSEW)
+        self.entry_frame.grid(row=3, sticky=tk.NSEW)
         self.text = tk.Text(self.entry_frame, width=30, height=10, wrap="word")
         self.scrollbar = ttk.Scrollbar(self.entry_frame)
 
-        self.scrollbar["command"] = self.text.yview  # Connect scrollbar to text
-        self.text.configure(yscrollcommand=self.scrollbar.set)  # Connect text to scrollbar
+        # Connect scrollbar to text area
+        self.scrollbar["command"] = self.text.yview
+        self.text.configure(yscrollcommand=self.scrollbar.set)
 
         self.scrollbar.grid(row=0, column=1, sticky=tk.NSEW)
         self.text.grid(row=0, column=0, sticky=tk.NSEW)
-        self.text.insert(0.0, "Delete this text, then enter one word per line.")
+        self.text.insert(
+            0.0,
+            "Delete this text, then enter one word per line."
+            )
 
         # Resize the entry frame around the text box
         self.entry_frame.rowconfigure(0, weight=1)
         self.entry_frame.columnconfigure(0, weight=1)
 
         # Go button
-        ttk.Button(self, text="Generate", command=self.generate_puzzle).grid(row=3, sticky=tk.NSEW)
+        ttk.Button(self, text="Generate", command=self.generate_puzzle)\
+            .grid(row=4, sticky=tk.NSEW)
 
         # Resize GUI about entry frame
         self.rowconfigure(2, weight=1)
@@ -124,7 +157,8 @@ class TkWindow(tk.Tk):
     def generate_puzzle(self):
         """Generate a puzzle from the input words"""
 
-        words_raw = self.text.get(0.0, tk.END).strip().upper()  # Read the entry area for words
+        # Read the entry area for words
+        words_raw = self.text.get(0.0, tk.END).strip().upper()
 
         # Checkpoint against invalid characters
         for letter in words_raw:
@@ -134,12 +168,20 @@ class TkWindow(tk.Tk):
 
         # Report and halt at any text problems
         if not words_raw:
-            mb.showerror("Invalid text", "Enter one word per line with no punctuation.")
+            mb.showerror(
+                "Invalid text",
+                "Enter one word per line with no punctuation."
+                )
             return
 
         # Generate the puzzle
         words = words_raw.split()
-        table = Generator.gen_word_search(words, directions=self.directions, size_fac=self.size_fac)
+        table = Generator.gen_word_search(
+            words,
+            directions=self.directions,
+            size_fac=self.size_fac,
+            intersect_bias=self.intersect_bias.get()
+            )
 
         # Render the puzzle
         text = Generator.render_puzzle(table)
